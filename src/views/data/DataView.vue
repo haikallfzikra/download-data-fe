2039f5101eca69c991dd69519d6a7255776c0dd0
haikallfzikra <haikallfzikra@gmail.com>
2025-12-02 08:41:17 +0700

    fix: comment download table

--- src/views/data/DataView.vue
+++ src/views/data/DataView.vue
@@ -237,7 +237,7 @@
               <td
                 class="px-3 py-2 border-b font-medium text-gray-900 whitespace-normal sm:whitespace-nowrap"
               >
-                AQMS {{ stationName }}
+                WQMS {{ stationName }}
               </td>
 
               <!-- Waktu -->
